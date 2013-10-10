09729a666948cd2af9f6b91a786d7ad5c5f51afd
kinterror <kinterror@dominator-hc4l>
2013-10-10 16:21:22 +0200

    Updating RedExt class: now using ConcurrentHashMap.

--- mcndfs/src/ndfs/mcndfs_extensions/RedExt.java
+++ mcndfs/src/ndfs/mcndfs_extensions/RedExt.java
@@ -1,31 +1,26 @@
 package ndfs.mcndfs_extensions;
 
-
-import java.util.Map;
-
 import graph.State;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 
 class RedExt {
 
-    private Map<State, Boolean> map;
+    private ConcurrentHashMap<State, Boolean> map;
 
-    
-    RedExt(Map<State, Boolean> map) {
+        
+    RedExt(ConcurrentHashMap<State, Boolean> map) {
         this.map = map;
     }
 
     
-    synchronized boolean isRed(State state) {
-        if(map.get(state)==null){//The first time a state is visited, it isn't in the map, so we add it and set its value to false
-            map.put(state, false);
-        }
+    boolean isRed(State state) {
+        this.map.putIfAbsent(state, Boolean.FALSE);
         return map.get(state);
     }
 
-
-    synchronized void setRed(State state, Boolean b) {
+    void setRed(State state, Boolean b) {
         map.put(state, b);
     }
 }
